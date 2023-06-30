946c02325e0f47db45db27ed91da7f841b5fbf9f
Hugo Araujo <araujohugo@msn.com>
2023-06-30 10:24:37 +0200

    ajout swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('register')
   async register(@Body() user: CreateUserDTO): Promise<CreateUserDTO> {
     return await this.authService.register(user);
   }
