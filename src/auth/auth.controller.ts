9adc4527b440412aa4c08d7f4726b8e0bb7bb683
Hugo Araujo <araujohugo@msn.com>
2023-06-30 10:58:05 +0200

    création compte

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDTO } from './dto/create-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiCreatedResponse({
+    type: Number,
+  })
   @Post('register')
-  async register(@Body() user: CreateUserDTO): Promise<CreateUserDTO> {
+  async register(@Body() user: CreateUserDTO): Promise<number> {
     return await this.authService.register(user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  async register(user: CreateUserDTO): Promise<any> {
-    return user;
+  async register(user: CreateUserDTO): Promise<number> {
+    const userExist = await this.userExist(user.email);
+    if (userExist) {
+      throw new ConflictException('Cet email est déjà associé à un compte');
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashpwd = await bcrypt.hash(user.password, salt);
+    const activationCode = this.activationCode();
+    await this.prisma.user.create({
+      data: {
+        email: user.email,
+        password: hashpwd,
+        codeActivate: activationCode,
+        refreshToken: 'dgferg',
+      },
+    });
+    return activationCode;
+  }
+
+  private async userExist(email: string): Promise<boolean> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (user) {
+      return true;
+    }
+    return false;
+  }
+
+  activationCode(): number {
+    return Math.floor(Math.random() * (10000 - 1000) + 1000);
   }
 }
