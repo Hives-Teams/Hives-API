db0b8e7708e9dba5cac059d25c18cd7a9d2b3dea
Hugo Araujo <araujohugo@msn.com>
2023-09-13 19:27:11 +0200

    finalement je le remet

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,29 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { BoardModule } from './board/board.module';
 import { TutoModule } from './tuto/tuto.module';
 import { MailModule } from './mail/mail.module';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   imports: [
     ConfigModule.forRoot(),
-
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     AuthModule,
     PrismaModule,
     BoardModule,
