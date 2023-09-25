843ce9755801b43a8624e950c3149aab526c2343
Hugo Araujo <araujohugo@msn.com>
2023-09-25 10:31:55 +0200

    suppression de compte inactif

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -15,6 +15,8 @@ import { ConnectUserDTO } from './dto/connect-user.dto';
 import { MailService } from 'src/mail/mail.service';
 import { ActivationCodeDTO } from './dto/activation-code.dto';
 import { IdUserDTO } from './dto/id-user.dto';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import dayjs from 'dayjs';
 
 @Injectable()
 export class AuthService {
@@ -284,5 +286,24 @@ export class AuthService {
       access_token: access_token,
       refresh_token: refresh_token,
     };
+  }
+
+  @Cron(CronExpression.EVERY_30_MINUTES)
+  async deleteInactiveAccount(): Promise<void> {
+    const user = await this.prisma.user.findMany();
+
+    user.forEach(async (u) => {
+      if (!u.activate) {
+        const diff = dayjs(Date.now()).diff(u.createdAt, 'day');
+        if (diff >= 1) {
+          console.log('suppression de ' + u.email);
+          await this.prisma.user.delete({
+            where: {
+              id: u.id,
+            },
+          });
+        }
+      }
+    });
   }
 }
