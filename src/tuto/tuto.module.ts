cb381be2a3c892ef39fa8808ee883cce2240f1db
Hugo Araujo <araujohugo@msn.com>
2023-09-29 14:00:20 +0200

    fix lien incorrect pour tiktok

--- src/tuto/tuto.module.ts
+++ src/tuto/tuto.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TutoService } from './tuto.service';
 import { TutoController } from './tuto.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, HttpModule],
   controllers: [TutoController],
   providers: [TutoService],
 })
