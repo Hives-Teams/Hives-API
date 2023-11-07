569adf6438e5a44a9e2e1f8c1e31bfb08732fad8
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2023-11-07 10:56:16 +0100

    HVS-155 - sélection multiple pour effacer  (#36)

--- src/tuto/tuto.controller.ts
+++ src/tuto/tuto.controller.ts
@@ -22,7 +22,9 @@ import { CreateTutoDTO } from './dto/create-tuto.dto';
 import { JwtGuard } from 'src/jwt/guards/jwt.guard';
 import { TutoDTO } from './dto/tuto.dto';
 import { DeleteTutoDTO } from './dto/delete-tuto.dto';
+import { DeleteTutosDTO } from './dto/delete-tutos.dto';
 
+@UseGuards(JwtGuard)
 @ApiTags('tuto')
 @ApiBearerAuth()
 @Controller('tuto')
@@ -38,7 +40,6 @@ export class TutoController {
     return await this.tutoService.reminderNotifications();
   }
 
-  @UseGuards(JwtGuard)
   @ApiOperation({
     summary: 'Affiche les réseaux sociaux des tutos enregistrée dans un board',
   })
@@ -62,7 +63,6 @@ export class TutoController {
     );
   }
 
-  @UseGuards(JwtGuard)
   @ApiOperation({
     summary: "Récupères les tutos d'un Board via son id",
   })
@@ -83,7 +83,6 @@ export class TutoController {
     return await this.tutoService.getTuto(req.user.sub, parseInt(idBoard));
   }
 
-  @UseGuards(JwtGuard)
   @ApiOperation({
     summary:
       "Récupère les tutos d'un Board via son idB + filtre réseau sociaux",
@@ -116,7 +115,6 @@ export class TutoController {
     );
   }
 
-  @UseGuards(JwtGuard)
   @ApiOperation({
     summary: 'Enregistre un ou plusieurs tuto dans un Board',
   })
@@ -132,15 +130,26 @@ export class TutoController {
     return await this.tutoService.setTutos(req.user.sub, createTuto);
   }
 
-  @UseGuards(JwtGuard)
   @ApiOperation({
     summary: 'Supprimer un tuto via son id',
   })
   @Delete()
   async deleteTuto(
     @Req() req: { user: TokenPayloadInterface },
-    @Body() board: DeleteTutoDTO,
+    @Body() tuto: DeleteTutoDTO,
   ): Promise<void> {
-    return await this.tutoService.deleteTuto(req.user.sub, board.id);
+    return await this.tutoService.deleteTuto(req.user.sub, tuto.id);
+  }
+
+  @ApiOperation({
+    summary: 'Supprime plusieurs tutos via leur id',
+  })
+  @Delete('array')
+  async deleteTutos(
+    @Req() req: { user: TokenPayloadInterface },
+    @Body() tutos: DeleteTutosDTO,
+  ): Promise<void> {
+    tutos.id = tutos.id.map((i) => parseInt(i as any));
+    return await this.tutoService.deleteTutos(req.user.sub, tutos.id);
   }
 }
