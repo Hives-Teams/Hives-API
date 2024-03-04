d9003115ffbf1143122372c15d58421640a5981a
huhulacolle <66556677+huhulacolle@users.noreply.github.com>
2024-03-04 23:17:22 +0100

    Update package.json and auth.controller.spec.ts, and mail.module.ts and mail.service.ts files

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,20 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtRefreshTokenStrategy } from 'src/jwt/strategies/jwt-refresh-token-strategy';
+import { JwtStrategy } from 'src/jwt/strategies/jwt.strategy';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { MailModule } from 'src/mail/mail.module';
+import { TokenDTO } from './dto/token.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule, JwtModule.register({}), MailModule],
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [AuthService, JwtRefreshTokenStrategy, JwtStrategy],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('should return an id of user', async () => {
+      const result: { id: number } = { id: 1 };
+      jest.spyOn(service, 'register').mockImplementation(async () => result);
+
+      expect(
+        await controller.register({
+          email: 'test@test.fr',
+          firstname: 'test',
+          lastname: 'test',
+          password: 'Abcdeg@',
+        }),
+      ).toBe(result);
+    });
+  });
+
+  describe('activate', () => {
+    it('should return a token', async () => {
+      const result: TokenDTO = {
+        access_token: 'test',
+        refresh_token: 'test',
+      };
+      jest.spyOn(service, 'activation').mockImplementation(async () => result);
+
+      expect(
+        await controller.activation({
+          code: 1,
+          id: 0,
+          idDevice: uuidv4(),
+        }),
+      ).toBe(result);
+    });
+  });
+
+  describe('login', () => {
+    it('should return a token', async () => {
+      const result: TokenDTO = {
+        access_token: 'test',
+        refresh_token: 'test',
+      };
+      jest.spyOn(service, 'login').mockImplementation(async () => result);
+
+      expect(
+        await controller.login({
+          email: '',
+          password: '',
+          idDevice: '',
+        }),
+      ).toBe(result);
+    });
   });
 });
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,52 +1,12 @@
 import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable, Logger } from '@nestjs/common';
-import { google } from 'googleapis';
-import { Options } from 'nodemailer/lib/smtp-transport';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MailService {
   constructor(private readonly mailerService: MailerService) {}
 
-  private async getTransport(): Promise<void> {
-    const OAuth2 = google.auth.OAuth2;
-
-    const oauth2Client = new OAuth2(
-      process.env.CLIENT_ID_MAIL,
-      process.env.CLIENT_SECRET_MAIL,
-      'https://developers.google.com/oauthplayground',
-    );
-    oauth2Client.setCredentials({
-      refresh_token: process.env.REFRESH_TOKEN_MAIL,
-    });
-
-    const accessToken: string = await new Promise((resolve, reject) => {
-      oauth2Client.getAccessToken((err, token) => {
-        if (err) {
-          Logger.error(err.response.data.error_description, 'mail.service');
-          reject(`Erreur lors de la création du compte : email non envoyé`);
-        }
-        resolve(token);
-      });
-    });
-
-    const config: Options = {
-      service: 'gmail',
-      auth: {
-        type: 'OAuth2',
-        user: process.env.EMAIL,
-        clientId: process.env.CLIENT_ID_MAIL,
-        clientSecret: process.env.CLIENT_SECRET_MAIL,
-        accessToken: accessToken,
-      },
-    };
-    this.mailerService.addTransporter('gmail', config);
-  }
-
   async sendConfirmationMail(email: string, code: number): Promise<void> {
-    await this.getTransport();
-
     await this.mailerService.sendMail({
-      transporterName: 'gmail',
       from: process.env.EMAIL,
       to: email,
       subject: 'Bienvenue dans la ruche Hives ! 🐝',
@@ -58,10 +18,7 @@ export class MailService {
   }
 
   async sendForgotPasswordMail(email: string, code: number): Promise<void> {
-    await this.getTransport();
-
     await this.mailerService.sendMail({
-      transporterName: 'gmail',
       from: process.env.EMAIL,
       to: email,
       subject: 'Mot de passe oublié',
@@ -73,10 +30,7 @@ export class MailService {
   }
 
   async sendRequestAccountDelete(email: string): Promise<void> {
-    await this.getTransport();
-
     await this.mailerService.sendMail({
-      transporterName: 'gmail',
       from: process.env.EMAIL,
       to: email,
       subject: 'Suppression de votre compte Hives',
