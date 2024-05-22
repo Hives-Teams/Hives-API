bf89e8e9ce630481c9dfe410378059e7d7446bec
huhulacolle <66556677+huhulacolle@users.noreply.github.com>
2024-05-22 08:53:42 +0000

    chore: Update .env file to include maintenance mode and message

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,4 +7,14 @@ export class AppService {
   getHello(): string {
     return null;
   }
+
+  async maintenance(): Promise<{ maintenance: boolean; message?: string }> {
+    if (Boolean(Number(process.env.MAINTENANCE))) {
+      return {
+        maintenance: Boolean(Number(process.env.MAINTENANCE)),
+        message: process.env.MAINTENANCE_MESSAGE,
+      };
+    }
+    return { maintenance: false };
+  }
 }
